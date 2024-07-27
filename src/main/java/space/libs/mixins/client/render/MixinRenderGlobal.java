af06d3b7ec444c5eb5c778d0b22e09cf2197b1bf
Howard ZHY <howardzhy128@163.com>
2024-07-27 15:58:40 +0800

    Fix RenderGlobal Mixin @Shadow Failed

--- src/main/java/space/libs/mixins/client/render/MixinRenderGlobal.java
+++ src/main/java/space/libs/mixins/client/render/MixinRenderGlobal.java
@@ -1,29 +1,32 @@
 package space.libs.mixins.client.render;
 
 import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.ViewFrustum;
 import net.minecraft.client.renderer.chunk.RenderChunk;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import space.libs.util.cursedmixinextensions.annotations.Public;
 
 @SuppressWarnings("unused")
-@Mixin(RenderGlobal.class)
+@Mixin(value = RenderGlobal.class, priority = 100)
 public abstract class MixinRenderGlobal {
+
+    @Shadow
+    private int renderDistanceChunks;
+
+    @Shadow
+    private ViewFrustum viewFrustum;
 
     @Shadow
     public static void drawSelectionBoundingBox(AxisAlignedBB boundingBox) {}
 
     @Shadow
     protected abstract org.lwjgl.util.vector.Vector3f getViewVector(Entity entityIn, double partialTicks);
-
-    @Shadow
-    private RenderChunk getRenderChunkOffset(BlockPos playerPos, RenderChunk renderChunkBase, EnumFacing facing) {
-        throw new AbstractMethodError();
-    }
 
     /** drawOutlinedBoundingBox */
     @Public
@@ -43,7 +46,13 @@ public abstract class MixinRenderGlobal {
 
     /** getRenderChunkOffset */
     public RenderChunk func_174973_a(BlockPos playerPos, RenderChunk renderChunkBase, EnumFacing facing) {
-        return this.getRenderChunkOffset(playerPos, renderChunkBase, facing);
+        BlockPos blockpos = renderChunkBase.getBlockPosOffset16(facing);
+        return MathHelper.abs_int(playerPos.getX() - blockpos.getX()) >
+            this.renderDistanceChunks * 16 ? null : (
+                blockpos.getY() >= 0 && blockpos.getY() < 256 ? (
+                MathHelper.abs_int(
+                    playerPos.getZ() - blockpos.getZ()) > this.renderDistanceChunks * 16 ? null : this.viewFrustum.getRenderChunk(blockpos)
+                ) : null );
     }
 
 }
