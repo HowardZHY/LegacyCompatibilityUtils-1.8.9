5cb6dbad10164ad53541843240b0115aa21157ca
Howard ZHY <howardzhy128@163.com>
2024-08-16 02:48:50 +0800

    Add some more classes

---
+++ src/main/java/net/minecraft/client/particle/EntityFireworkStarterFX.java
@@ -0,0 +1,10 @@
+package net.minecraft.client.particle;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class EntityFireworkStarterFX extends EntityFirework.StarterFX {
+    public EntityFireworkStarterFX(World p_i46464_1_, double p_i46464_2_, double p_i46464_4_, double p_i46464_6_, double p_i46464_8_, double p_i46464_10_, double p_i46464_12_, EffectRenderer p_i46464_14_, NBTTagCompound p_i46464_15_) {
+        super(p_i46464_1_, p_i46464_2_, p_i46464_4_, p_i46464_6_, p_i46464_8_, p_i46464_10_, p_i46464_12_, p_i46464_14_, p_i46464_15_);
+    }
+}
