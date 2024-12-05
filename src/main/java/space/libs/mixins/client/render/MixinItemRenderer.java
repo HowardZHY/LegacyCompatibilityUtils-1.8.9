ce6c846c4bfbd6890beeab528b6e58d654adaffc
Howard ZHY <howardzhy128@163.com>
2024-12-06 01:10:50 +0800

    Fix possible NPE

--- src/main/java/space/libs/mixins/client/render/MixinItemRenderer.java
+++ src/main/java/space/libs/mixins/client/render/MixinItemRenderer.java
@@ -3,12 +3,12 @@ package space.libs.mixins.client.render;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.RenderItem;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumWorldBlockLayer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -22,18 +22,22 @@ public abstract class MixinItemRenderer {
     }
 
     /** itemRenderer */
-    private RenderItem field_178112_h;
+    @Shadow
+    private @Final net.minecraft.client.renderer.entity.RenderItem itemRenderer;
 
+    private net.minecraft.client.renderer.RenderItem field_178112_h;
+
+    /** renderItemSide */
     public void func_187462_a(EntityLivingBase entity, ItemStack stack, ItemCameraTransforms.TransformType type, boolean b) {
         if (stack != null) {
             Item item = stack.getItem();
             Block block = Block.getBlockFromItem(item);
             GlStateManager.pushMatrix();
-            boolean flag = (this.field_178112_h.shouldRenderItemIn3D(stack) && isBlockTranslucent(block));
+            boolean flag = (this.itemRenderer.shouldRenderItemIn3D(stack) && isBlockTranslucent(block));
             if (flag) {
                 GlStateManager.depthMask(false);
             }
-            this.field_178112_h.renderItem(stack, type);
+            this.itemRenderer.renderItem(stack, type);
             if (flag) {
                 GlStateManager.depthMask(true);
             }
