729a25b993eedf9e0f7c21e123a5b6067475a399
Howard ZHY <howardzhy128@163.com>
2024-12-09 03:49:12 +0800

    Prevent MixinAttributes$1

--- src/main/java/space/libs/mixins/client/forge/MixinAttributes.java
+++ src/main/java/space/libs/mixins/client/forge/MixinAttributes.java
@@ -10,10 +10,10 @@ import net.minecraft.client.renderer.vertex.VertexFormatElement;
 import net.minecraftforge.client.model.Attributes;
 import net.minecraftforge.client.model.TRSRTransformation;
 import org.spongepowered.asm.mixin.Mixin;
+import space.libs.core.CompatLibCore;
 import space.libs.interfaces.IVertexFormatElement;
 import space.libs.util.cursedmixinextensions.annotations.Public;
 
-import javax.vecmath.Tuple4f;
 import javax.vecmath.Vector4f;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
@@ -27,30 +27,28 @@ public class MixinAttributes {
         if (e.getElementCount() > ns.length && fill == null) throw new IllegalArgumentException("not enough elements");
         Number n;
         for (int i = 0; i < e.getElementCount(); i++) {
-            if (i < ns.length) n = ns[i];
-            else n = fill;
-            switch (e.getType()) {
-                case BYTE:
-                    buf.put(normalize ? (byte)(n.floatValue() / (Byte.MAX_VALUE - 1)) : n.byteValue());
-                    break;
-                case UBYTE:
-                    buf.put(normalize ? (byte)(n.floatValue() / ((byte) -1)) : n.byteValue());
-                    break;
-                case SHORT:
-                    buf.putShort(normalize ? (short)(n.floatValue() / (Short.MAX_VALUE - 1)) : n.shortValue());
-                    break;
-                case USHORT:
-                    buf.putShort(normalize ? (short)(n.floatValue() / ((short) -1)) : n.shortValue());
-                    break;
-                case INT:
-                    buf.putInt(normalize ? (int)(n.doubleValue() / (Integer.MAX_VALUE - 1)) : n.intValue());
-                    break;
-                case UINT:
-                    buf.putInt(normalize ? (int)(n.doubleValue() / ((int) - 1)) : n.intValue());
-                    break;
-                case FLOAT:
-                    buf.putFloat(n.floatValue());
-                    break;
+            if (i < ns.length) {
+                n = ns[i];
+            } else {
+                n = fill;
+            }
+            final int o = e.getType().ordinal();
+            if (o == 1) {
+                buf.put(normalize ? (byte) (n.floatValue() / (Byte.MAX_VALUE - 1)) : n.byteValue());
+            } else if (o == 2) {
+                buf.put(normalize ? (byte) (n.floatValue() / ((byte) -1)) : n.byteValue());
+            } else if (o == 3) {
+                buf.putShort(normalize ? (short)(n.floatValue() / (Short.MAX_VALUE - 1)) : n.shortValue());
+            } else if (o == 4) {
+                buf.putShort(normalize ? (short)(n.floatValue() / ((short) -1)) : n.shortValue());
+            } else if (o == 5) {
+                buf.putInt(normalize ? (int)(n.doubleValue() / (Integer.MAX_VALUE - 1)) : n.intValue());
+            } else if (o == 6) {
+                buf.putInt(normalize ? (int)(n.doubleValue() / - 1) : n.intValue());
+            } else if (o == 7) {
+                buf.putFloat(n.floatValue());
+            } else {
+                CompatLibCore.LOGGER.error("Unknown VertexFormatElement EnumType:" + o);
             }
         }
     }
