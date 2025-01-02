d8e306ea9ba654ae25fead8ebbaba1a8542ead39
Howard ZHY <howardzhy128@163.com>
2025-01-02 23:41:28 +0800

    minor cleanup

--- src/main/java/space/libs/mixins/client/render/entity/MixinEntityRenderer.java
+++ src/main/java/space/libs/mixins/client/render/entity/MixinEntityRenderer.java
@@ -9,6 +9,15 @@ import org.spongepowered.asm.mixin.Shadow;
 @Mixin(EntityRenderer.class)
 public abstract class MixinEntityRenderer {
 
+    /** 1.7.10 Unused */
+    public float field_78495_O = 0.0F;
+
+    public float camRoll = 0.0F;
+
+    public float field_78505_P = 0.0F;
+
+    public float prevCamRoll = 0.0F;
+
     @Shadow
     public void updateCameraAndRender(float partialTicks, long nanoTime) {}
 
